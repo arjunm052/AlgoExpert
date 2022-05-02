ab1e9f5cce27edffe6ccec10491ee83b5e51ee02
Arjun Meena <arjunmeena@Arjuns-MacBook-Pro.local>
2022-05-02 12:57:41 +0530

    Two Number Sum

---
+++ Arrays/Two Number Sum/twoNumber.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class twoNumber{
+    public static void main(String[] args) {
+        
+    }
+
+    //TC - O(N)  SC - O(N)
+    //Use a HashSet to keep track of all the visited numbers. Calculate target-arr[i] 
+    //and check if it exists in hashset. If it exists then we have our pair. 
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+        HashSet<Integer> hs = new HashSet<Integer>();
+            int res[] = new int[2];
+            for(int i=0;i<array.length;i++){
+                if(hs.contains(targetSum-array[i])){
+                    res[0]=array[i];
+                    res[1]=targetSum-array[i];
+                    return res;
+                }else{
+                    hs.add(array[i]);
+                }
+            }
+        return new int[0];
+      }
+
+    //TC - O(NLOGN)    SC - O(1)
+    //Sort the array and use the two pointer approach to check for pair.
+    public static int[] twoNumberSum2(int[] array, int targetSum) {
+		Arrays.sort(array);
+        int left = 0;
+		int right = array.length-1;
+		while(left<right){
+			if(array[left]+array[right]==targetSum){
+				return new int[]{array[left],array[right]};
+			}
+			else if(array[left]+array[right]<targetSum){
+				left++;
+			}
+			else{
+				right--;
+			}
+			
+		}
+        return new int[0];
+    }
+}
+
+
+
+
