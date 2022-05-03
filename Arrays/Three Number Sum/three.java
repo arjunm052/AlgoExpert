43018e8f3f8ea43e9b5a28f4f487abaac47fd678
Arjun <singh.arjun127@gmail.com>
2022-05-03 23:50:54 +0530

    Add Comments Three Number Sum

--- Arrays/Three Number Sum/three.java
+++ Arrays/Three Number Sum/three.java
@@ -9,15 +9,19 @@ public class three {
     //Sort the array and traverse it. For each number in iterattion, do a two pointer approach to find
     //all the pairs when added with our current iteration value, would sum up to the targetSum and add them to 
     //the result.
-    public static List<Integer[]> threeNumberSum(int[] array, int targetSum) {
-        ArrayList<Integer[]> res = new ArrayList<Integer[]>();
+        public static List<Integer[]> threeNumberSum(int[] array, int targetSum) {
+            ArrayList<Integer[]> res = new ArrayList<Integer[]>();
+            //If Length of array is less than two then we can't have a triplet
             if(array.length<2){
                 return res;
             }
+            //Sort the array
             Arrays.sort(array);
+            //Traverse through the array and for every element initialize start and end pointers as i+1 and arr.length-1
             for(int i=0;i<array.length-2;i++){
                 int start = i+1;
                 int end = array.length-1;
+                //use the two pointer approach to find the pair that when added with arr[i] sums up to the targetSum
                 while(start<end){
                     if((array[i]+array[start]+array[end])==targetSum){
                         res.add(new Integer[]{array[i],array[start],array[end]});
@@ -30,7 +34,7 @@ public class three {
                     }
                 }
             }
-            
-        return res;
-      }
+            //return the final result
+            return res;
+        }
 }
