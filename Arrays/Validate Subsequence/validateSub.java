c0e95a23b181696ee108cf8d6085e2584e9b7a91
arjunm052 <51656770+arjunm052@users.noreply.github.com>
2022-05-03 23:31:32 +0530

    Add Comments Validate Subsequence

--- Arrays/Validate Subsequence/validateSub.java
+++ Arrays/Validate Subsequence/validateSub.java
@@ -11,14 +11,21 @@ public class validateSub {
     //integer in the potential subsequence. Continue this process until you find either every integer
     //in subsequence or you reach the end of the main array.
     public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+        //i points to the start of main array
         int i = 0;
-            int j = 0;
-            while(i<array.size() && j<sequence.size()){
-                if(array.get(i)==sequence.get(j)){
-                    j++;
-                }
-                i++;
+        //j points to the start of potential subsequence
+        int j = 0;
+        
+        //Traverse the main array and look for the first character of potential subsequence
+        while(i<array.size() && j<sequence.size()){
+            //If we find the matching character then just increase the subsequence pointer and look for next character
+            if(array.get(i)==sequence.get(j)){
+                j++;
             }
+            //Also keep increasing the main pointer array
+            i++;
+        }
+        //If j is equal to subsequence length then we have a valid subsequence
         return (j==sequence.size());
       }
 
