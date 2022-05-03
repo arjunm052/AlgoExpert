01f338e8c6de4df0113d9ca0a48443b65dacfe62
arjunm052 <51656770+arjunm052@users.noreply.github.com>
2022-05-03 23:39:05 +0530

    Add Comments Tournament Winner

--- Arrays/Tournament Winner/tourna.java
+++ Arrays/Tournament Winner/tourna.java
@@ -8,11 +8,13 @@ public class tourna {
     //TC - O(N)  SC - O(K)
     //Create a HashMap that will be the points table of the tournament. Go through the matches and update
     //the hashmap. Then find the team with the most points. 
-    public String tournamentWinner(
-        ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results) {
-        HashMap<String,Integer> map = new HashMap<String,Integer>();
+    public String tournamentWinner(ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results) {
+            //Use a hashmap to keep track of points of all teams
+            HashMap<String,Integer> map = new HashMap<String,Integer>();
+            //Maxpoints and res stores the points and name of the winner
             int maxPoints = -1;
             String res = "";
+            //Fill up the HashMap by awarding 3 points to the winning team in HashMap
             for(int i = 0; i<competitions.size(); i++){
                 ArrayList<String> match = competitions.get(i);
                 int winner = results.get(i);
@@ -20,16 +22,18 @@ public class tourna {
                 map.put(winTeam,map.getOrDefault(winTeam,0)+3);
             }
             
+            //Traverse through the HashMap and find the team with the maximum points
             for (Map.Entry mapElement : map.entrySet()) {
             String key = (String)mapElement.getKey();
             int value =  (int)mapElement.getValue();
-                            if(value>maxPoints){
-                                res = key;
-                                maxPoints = value;
-                            }          
+                 if(value>maxPoints){
+                      res = key;
+                      maxPoints = value;
+                 }          
             }
             
+            //Return the final result
             return res;
-    }
+     }
 
 }
