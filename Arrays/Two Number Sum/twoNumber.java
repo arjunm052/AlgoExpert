603797072d1bf6af093262d703c3185e83f3ccf2
arjunm052 <51656770+arjunm052@users.noreply.github.com>
2022-05-03 23:27:29 +0530

    Add Comments Two Number Sum

--- Arrays/Two Number Sum/twoNumber.java
+++ Arrays/Two Number Sum/twoNumber.java
@@ -9,17 +9,23 @@ public class twoNumber{
     //Use a HashSet to keep track of all the visited numbers. Calculate target-arr[i] 
     //and check if it exists in hashset. If it exists then we have our pair. 
     public static int[] twoNumberSum(int[] array, int targetSum) {
+	//Use HashSet to keep track of all visited numbers
         HashSet<Integer> hs = new HashSet<Integer>();
             int res[] = new int[2];
+	    //Traverse through all the elements of the array
             for(int i=0;i<array.length;i++){
+		//Check if we have already visited targetSum-array[i]
                 if(hs.contains(targetSum-array[i])){
+		    //If we have store the result and return it.	
                     res[0]=array[i];
                     res[1]=targetSum-array[i];
                     return res;
                 }else{
+		    //If we have not visited targetSum-array[i], just store the array[i] in HashSet and move on
                     hs.add(array[i]);
                 }
             }
+	//return empty array if no pair exists
         return new int[0];
       }
 
