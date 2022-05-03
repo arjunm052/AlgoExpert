1c0f40891a88a6b127fca0303e197e325b72b08e
Arjun <singh.arjun127@gmail.com>
2022-05-04 00:19:42 +0530

    Add Comments Monotonic Array

--- Arrays/Monotonic Array/monton.java
+++ Arrays/Monotonic Array/monton.java
@@ -8,16 +8,18 @@ public class monton {
     //TC - O(N)  SC - O(1)
     //Check if its either Monotonic Increasing or is it Montonic Deccreasing and their their OR operation
     public static boolean isMonotonic(int[] array) {
+        //Base case
         if(array.length==1 || array.length==0){
-                return true;
-        }
-            
-            boolean isInc = incMonotonic(array);
-            boolean isDec = decMonotonic(array);
-            
+            return true;
+        }    
+        //Find whether array is monotonically increasing or decreasing or neither
+        boolean isInc = incMonotonic(array);
+        boolean isDec = decMonotonic(array);
+    
+        //return the final result
         return isInc | isDec;
       }
-        
+        //Function to check whether the array is monotonically increasing
         public static boolean incMonotonic(int[] arr){
             for(int i=1;i<arr.length;i++){
                 if(arr[i]<arr[i-1]){
@@ -27,6 +29,7 @@ public class monton {
             return true;
         }
         
+        //Function to check whether the array is monotonically decreasing
         public static boolean decMonotonic(int[] arr){
             for(int i=1;i<arr.length;i++){
                 if(arr[i]>arr[i-1]){
