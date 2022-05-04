7910506eca221bbb1f6580b71d515b2508958e89
arjunm052 <51656770+arjunm052@users.noreply.github.com>
2022-05-04 23:53:25 +0530

    Added Comments Non Constructible Change

--- Arrays/Non Constructible Change/noncons.java
+++ Arrays/Non Constructible Change/noncons.java
@@ -5,21 +5,21 @@ public class noncons {
         
     }
 
-    //TC - O(N)   SC - O(1)
+    //TC - O(NLogN)   SC - O(1)
     public int nonConstructibleChange(int[] coins) {
         //Sort the given input array
-		Arrays.sort(coins);
+	Arrays.sort(coins);
         //coinChange keeps track of the minimum amount that we can make
         int coinChange = 0;
         //We traverse all the coins and if we find a coin that is bigger than coinChange+1, then 
         //the minimum amount we cannot construct is going to be coinChange+1
-		for(int value: coins){
-			if(value>coinChange+1){
-				return coinChange+1;
-			}
-            //If it is less than coinchange+1 then we just add its value to the coinChange and carry on
-			coinChange+=value;
+	for(int value: coins){
+		if(value>coinChange+1){
+			return coinChange+1;
 		}
+            //If it is less than coinchange+1 then we just add its value to the coinChange and carry on
+	    coinChange+=value;
+	}
         //Return the final result
         return coinChange+1;
     }
